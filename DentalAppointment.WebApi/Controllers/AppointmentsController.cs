bd2d8809d791f73fcca07d227709b039ba61fc12
Sorin Giurgica <H3XM@it7.eurofins.com>
2024-08-16 17:52:13 +0300

    Initial commit

---
+++ DentalAppointment.WebApi/Controllers/AppointmentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DentalAppointment.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController(IAppointmentService appointmentService) : ControllerBase
+    {
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetAppointment(int id)
+    {
+        var appointment = await appointmentService.GetAppointmentByIdAsync(id);
+        if (appointment == null)
+        {
+            return NotFound();
+        }
+        return Ok(appointment);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAppointment(AppointmentDto appointmentDto)
+    {
+        var result = await appointmentService.CreateAppointmentAsync(appointmentDto);
+        if (!result.IsSuccess)
+        {
+            return BadRequest(result.ErrorMessage);
+        }
+        return Ok(result.Appointment);
+    }
+}
